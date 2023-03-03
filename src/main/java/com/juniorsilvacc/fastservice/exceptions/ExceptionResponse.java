682019db104f65f5c40e034f79fad900863d048e
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-03-03 16:29:35 -0300

    exception handling

---
+++ src/main/java/com/juniorsilvacc/fastservice/exceptions/ExceptionResponse.java
@@ -0,0 +1,44 @@
+package com.juniorsilvacc.fastservice.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date timestamp;
+	private String message;
+	private String path;
+	
+	public ExceptionResponse(Date timestamp, String message, String path) {
+		this.timestamp = timestamp;
+		this.message = message;
+		this.path = path;
+	}
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+}
--- src/main/java/com/juniorsilvacc/fastservice/services/CategoryService.java
+++ src/main/java/com/juniorsilvacc/fastservice/services/CategoryService.java
@@ -10,6 +10,7 @@ import com.juniorsilvacc.fastservice.controllers.CategoryController;
 import com.juniorsilvacc.fastservice.domain.Category;
 import com.juniorsilvacc.fastservice.dto.CategoryDTO;
 import com.juniorsilvacc.fastservice.repositories.CategoryRepository;
+import com.juniorsilvacc.fastservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -18,14 +19,14 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	public CategoryDTO findById(Integer id) {
-		Category entity = repository.findById(id).get();
-		
+		Category entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException(String.format("Categoria com id: %d não encontrado", id)));
+	
 		CategoryDTO dto = new CategoryDTO(entity);
 		
 		dto.add(linkTo(methodOn(CategoryController.class).findById(id)).withSelfRel());
 		
 		return dto;
-		
 	}
 
 }
