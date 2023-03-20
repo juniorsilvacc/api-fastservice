46800806f99689f6b120787c0e5a9fb7e299638b
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-03-20 01:20:00 -0300

    finalizing the implementation of the JwtTokenProvider

--- src/main/java/com/juniorsilvacc/fastservice/securityJwt/JwtTokenProvider.java
+++ src/main/java/com/juniorsilvacc/fastservice/securityJwt/JwtTokenProvider.java
@@ -6,13 +6,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.juniorsilvacc.fastservice.domain.dtos.security.TokenDTO;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class JwtTokenProvider {
@@ -21,7 +29,7 @@ public class JwtTokenProvider {
 	private String secretKey = "secret";
 	
 	@Value("${jwt.expiration=3600000}")
-	private long validatyInMilliSeconds = 3600000;
+	private long validatyInMilliSeconds = 3600000; // 1h
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -42,15 +50,57 @@ public class JwtTokenProvider {
 		
 		return new TokenDTO(email, true, now, validity, accessToken, refreshToken);
 	}
+	
+	private String getAccessToken(String email, List<String> roles, Date now, Date validity) {
+		String issuerURL = ServletUriComponentsBuilder
+				.fromCurrentContextPath().build().toUriString();
+		
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validity)
+				.withSubject(email)
+				.withIssuer(issuerURL)
+				.sign(algorithm)
+				.strip();
+	}
 
 	private String getRefreshToken(String email, List<String> roles, Date now) {
-		// TODO Auto-generated method stub
-		return null;
+		Date validityRefreshToken = new Date(now.getTime() + (validatyInMilliSeconds * 3));
+		
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validityRefreshToken)
+				.withSubject(email)
+				.sign(algorithm)
+				.strip();
+	}
+	
+	public Authentication getAuthentication(String token) {
+		DecodedJWT decodedJWT = decodedToken(token);
+		
+		UserDetails userDetails = this.userDetailsService
+				.loadUserByUsername(decodedJWT.getSubject());
+		
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 	}
 
-	private String getAccessToken(String email, List<String> roles, Date now, Date validity) {
-		// TODO Auto-generated method stub
+	private DecodedJWT decodedToken(String token) {
+		Algorithm alg = Algorithm.HMAC256(secretKey.getBytes());
+		JWTVerifier verifier = JWT.require(alg).build();
+		DecodedJWT decodedJWT = verifier.verify(token);
+		return decodedJWT;
+	}
+	
+	public String resolveToken(HttpServletRequest req) {
+		String bearerToken = req.getHeader("Authorization");
+		
+		// Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWFuZHJvIiwicm9...
+		// Se não for null e começa com Bearer, faz o substring
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring("Bearer ".length());
+		}
 		return null;
 	}
-
 }
