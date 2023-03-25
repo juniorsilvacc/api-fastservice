db5505c71c5427ca4c42afb07a019277b21f1c4a
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-03-25 01:55:36 -0300

    service responsible for storing files on disk

--- src/main/java/com/juniorsilvacc/fastservice/config/OpenApiConfig.java
+++ src/main/java/com/juniorsilvacc/fastservice/config/OpenApiConfig.java
@@ -3,16 +3,28 @@ package com.juniorsilvacc.fastservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
+@SecurityScheme(
+	    name = "bearerAuth",
+	    type = SecuritySchemeType.HTTP,
+	    bearerFormat = "JWT",
+	    scheme = "bearer"
+)
 public class OpenApiConfig {
+	
+	final String securitySchemeName = "bearerAuth";
 	
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
+			.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
 			.info(new Info()
 				.title("RESTFul API - Fast Service")
 				.version("v1")
