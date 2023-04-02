94123149744d9e3d4a51cfb15c1df35df11a130d
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-04-02 14:27:50 -0300

    test for category service class findById method object not found

--- src/test/java/com/juniorsilvacc/fastservice/unittests/mockito/services/CategoryServiceTest.java
+++ src/test/java/com/juniorsilvacc/fastservice/unittests/mockito/services/CategoryServiceTest.java
@@ -3,6 +3,7 @@ package com.juniorsilvacc.fastservice.unittests.mockito.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -11,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,6 +19,7 @@ import com.juniorsilvacc.fastservice.domain.dtos.CategoryDTO;
 import com.juniorsilvacc.fastservice.domain.entities.Category;
 import com.juniorsilvacc.fastservice.repositories.CategoryRepository;
 import com.juniorsilvacc.fastservice.services.CategoryService;
+import com.juniorsilvacc.fastservice.services.exceptions.ObjectNotFoundException;
 
 @SpringBootTest
 class CategoryServiceTest {
@@ -26,6 +27,8 @@ class CategoryServiceTest {
 	private static final Integer ID = 1;
 	private static final String NAME = "Hambúrguers";
 	private static final String DESCRIPTION = "O hambúrguer foi o mais pedido no país durante o último ano.";
+	
+	private static final String OBJECT_NOT_FOUND = "Categoria com id: %d não encontrado " + ID;
 	
 	@InjectMocks
 	private CategoryService service;
@@ -57,6 +60,19 @@ class CategoryServiceTest {
 		assertEquals(1, result.getId());
 		assertEquals("Hambúrguers", result.getName());
 		assertEquals("O hambúrguer foi o mais pedido no país durante o último ano.", result.getDescription());
+	}
+	
+	@Test
+	void whenFindByIdThenReturnObjectNotFoundException() {
+		when(repository.findById(ID))
+        	.thenThrow(new ObjectNotFoundException(OBJECT_NOT_FOUND));
+		
+		try {
+			service.findById(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+            assertEquals("Categoria com id: %d não encontrado " + ID, e.getMessage());
+		}
 	}
 
 	@Test
