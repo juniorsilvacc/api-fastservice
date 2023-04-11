3f8f41bf16f3047b90aa0626ad807d4c235fd587
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-04-11 18:16:57 -0300

    test for itemOrder service class findById method objetct not exists

--- src/test/java/com/juniorsilvacc/fastservice/unittests/services/ItemOrderServiceTest.java
+++ src/test/java/com/juniorsilvacc/fastservice/unittests/services/ItemOrderServiceTest.java
@@ -1,5 +1,6 @@
 package com.juniorsilvacc.fastservice.unittests.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -16,6 +17,7 @@ import com.juniorsilvacc.fastservice.domain.entities.Order;
 import com.juniorsilvacc.fastservice.domain.entities.Product;
 import com.juniorsilvacc.fastservice.repositories.ItemOrderRepository;
 import com.juniorsilvacc.fastservice.services.ItemOrderService;
+import com.juniorsilvacc.fastservice.services.exceptions.ObjectNotFoundException;
 
 @SpringBootTest
 class ItemOrderServiceTest {
@@ -24,6 +26,8 @@ class ItemOrderServiceTest {
 	private static final Integer AMOUNT = 3;
 	private Order ORDER;
 	private Product PRODUCT;
+	
+	private static final String OBJECT_NOT_FOUND = "Item com id: %d não encontrado " + ID;
 	
 	@InjectMocks
 	private ItemOrderService service;
@@ -44,13 +48,26 @@ class ItemOrderServiceTest {
 	}
 	
 	@Test
-	void deleteItemWithSuccess() {
+	void removeItemWithSuccess() {
 		ItemOrder entity = new ItemOrder(ID, AMOUNT, ORDER, PRODUCT);
 		entity.setId(1);
 		
 		when(repository.findById(1)).thenReturn(Optional.of(itemOrder));
 		
 		repository.delete(itemOrder);
+	}
+	
+	@Test
+	void whenFindByIdThenReturnObjectNotFoundException() {
+		when(repository.findById(ID))
+        	.thenThrow(new ObjectNotFoundException(OBJECT_NOT_FOUND));
+		
+		try {
+			service.remove(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+            assertEquals(OBJECT_NOT_FOUND, e.getMessage());
+		}
 	}
 	
 	void inputItemOrder() {
