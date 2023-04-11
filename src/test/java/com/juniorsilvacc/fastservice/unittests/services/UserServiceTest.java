7858b7fe9e895b5b3f4eb4662fff583756d85ae5
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-04-11 01:20:51 -0300

    test for user service class updateAvatar method

--- src/test/java/com/juniorsilvacc/fastservice/unittests/services/UserServiceTest.java
+++ src/test/java/com/juniorsilvacc/fastservice/unittests/services/UserServiceTest.java
@@ -2,19 +2,24 @@ package com.juniorsilvacc.fastservice.unittests.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.juniorsilvacc.fastservice.domain.dtos.UserDTO;
 import com.juniorsilvacc.fastservice.domain.entities.Permission;
 import com.juniorsilvacc.fastservice.domain.entities.User;
+import com.juniorsilvacc.fastservice.repositories.UserRepository;
 import com.juniorsilvacc.fastservice.services.UserService;
 
 @SpringBootTest
@@ -33,8 +38,10 @@ class UserServiceTest {
     
     @Autowired
     private UserService service;
+    
+    @Mock
+	private UserRepository repository;
 	
-	@SuppressWarnings("unused")
 	private User user;
 	
 	@BeforeEach
@@ -61,6 +68,29 @@ class UserServiceTest {
 	    assertEquals(EMAIL, response.getEmail());
 	    assertEquals(CPF, response.getCpf());
 	    assertEquals(AVATAR, response.getAvatar());
+	}
+	
+	@Test
+	void whenUpdatingUserReturnsSuccessfully() {
+		User persisted = new User(ID, USER_NAME, EMAIL, CPF, AVATAR, PASSWORD, PERMISSION);
+		
+		MockMultipartFile multipartFile = new MockMultipartFile("file",
+                "fresh-pepper.jpg",
+                "image/png, image/jpg, image/jpeg",
+                "random image".getBytes());
+		
+		when(repository.findById(ID)).thenReturn(Optional.of(user));
+		
+		UserDTO response = service.uploadAvatar(persisted, multipartFile);
+		response.setAvatar("fresh-pepper.jpg");
+		
+	    assertNotNull(response);
+	    assertNotNull(response.getId());
+	    
+	    assertEquals(UserDTO.class, response.getClass());
+	    
+	    assertEquals(ID, response.getId());
+	    assertEquals("fresh-pepper.jpg", response.getAvatar());
 	}
 	
 	private void inputUser() {
